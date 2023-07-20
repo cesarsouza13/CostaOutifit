23ef35c07be6aedfde20ef54e76b5ffad0520dac
unknown <cesardanielmg@hotmail.com>
2023-07-19 23:52:18 -0300

    atualizando diretorio

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import store from './store'
-import Router from 'routes';
+import Router from './routes.js';
 import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
